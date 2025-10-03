cc903bd887107f3a0fa915ff7d7ece825aa58916
Gonzalo <gonzalo@peepos.edu>
2025-10-03 18:13:02 -0500

    Flatten: mover contenido de '02-25-main' al raíz

--- 02-25-main/src/components/ui/index.ts
+++
@@ -1,7 +0,0 @@
-export { Card } from '@/components/common/Card';
-export { Button } from '@/components/common/Button';
-export { DataTable } from '@/components/common/DataTable';
-export { Filter } from '@/components/common/Filter';
-export { Form, FormField, Input, TextArea, Select } from '@/components/common/Form';
-export { Toast, ToastProvider, useToast } from '@/components/common/Toast';
-export { Page } from '@/components/common/Page';
