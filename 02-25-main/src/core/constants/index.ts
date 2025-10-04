c371c506121ac06054651c311a4c1dc1a3b9e5bd
goncho07 <angelvega0724@gmail.com>
2025-10-03 19:18:51 -0500

    Delete 02-25-main directory

--- 02-25-main/src/core/constants/index.ts
+++
@@ -1,5 +0,0 @@
-// Export all constants from a single entry point
-export * from './system';
-export * from './theme';
-export * from './api';
-export * from './types';
--- 02-25-main/src/hooks/index.ts
+++
@@ -1,4 +0,0 @@
-export { useApi, usePagination, useMutation } from './useApi';
-export { useAuth } from './useAuth';
-export { useDebounce } from './useDebounce';
-export { useOnlineStatus } from './useOnlineStatus';
